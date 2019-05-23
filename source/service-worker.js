9cb735063fcaede86bfd9f3f6d221cedecdbff4c
real-neo <questionyugood@gmail.com>
2019-05-23 01:31:23 +0100

    Site backup 2019-05-23 01:31

---
+++ source/service-worker.js
@@ -0,0 +1,105 @@
+// Copyright 2016 Google Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const cacheName = 'real-neo';
+const filesToCache = [
+    '/',
+    '/js/affix.js?v=7.1.0',
+    '/js/exturl.js?v=7.1.0',
+    '/lib/jquery/index.js?v=2.1.3',
+    '/lib/fancybox/source/jquery.fancybox.pack.js',
+    '/lib/jquery_lazyload/jquery.lazyload.js?v=1.9.7',
+    '/js/motion.js?v=7.1.0',
+    '/js/next-boot.js?v=7.1.0',
+    '/js/schemes/pisces.js?v=7.1.0',
+    '/lib/quicklink/dist/quicklink.umd.js',
+    '/js/utils.js?v=7.1.0',
+    '/lib/velocity/velocity.min.js?v=1.2.1',
+    '/lib/velocity/velocity.ui.min.js?v=1.2.1',
+    '/uploads/avatar.jpg',
+    '/images/cc-by-nc-sa.svg',
+    '/lib/font-awesome/css/font-awesome.min.css?v=4.6.2',
+    '/lib/fancybox/source/jquery.fancybox.css',
+    '/css/main.css?v=7.1.0',
+    '/lib/font-awesome/fonts/fontawesome-webfont.woff2?v=4.7.0',
+    '/favicon.ico',
+    '/offline.html'
+];
+
+/**
+ * installation event: it adds all the files to be cached
+ */
+self.addEventListener('install', function (e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+/**
+ * activation of service worker: it removes all cashed files if necessary
+ */
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log('[Service Worker] Fetch', event.request.url);
+    var blogDetail = /https\:\/\/real\-neo\.me\/[\s\S]*\.html/i;
+    var blog = /https\:\/\/real\-neo\.me[\s\S]*/i;
+    if (event.request.url.match(blogDetail)) {
+        event.respondWith(async function () {
+            const cachedResponse = await caches.match(event.request);
+            if (cachedResponse) return cachedResponse;
+
+            try {
+                const cache = await caches.open('ins-dynamic');
+                const networkResponsePromise = fetch(event.request);
+                const networkResponse = await networkResponsePromise;
+                await cache.put(event.request, networkResponse.clone());
+                return networkResponsePromise;
+            } catch (e) {
+                return caches.match('/offline.html');
+            }
+        }());
+    } else if (event.request.url.match(blog)) {
+        event.respondWith(async function () {
+            try {
+                const cache = await caches.open('real-neo');
+                const networkResponsePromise = fetch(event.request);
+                const networkResponse = await networkResponsePromise;
+                await cache.put(event.request, networkResponse.clone());
+                return networkResponsePromise;
+            } catch (e) {
+                const cachedResponse = await caches.match(event.request);
+                if (cachedResponse) return cachedResponse;
+                return caches.match('/offline.html');
+            }
+        }());
+    }
+});
